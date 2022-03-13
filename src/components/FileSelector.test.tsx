4a4892e0b97d283ecde36c6c8b6fb95a6e09f938
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2022-03-13 10:32:43 -0400

    Add JSON validation

--- src/components/FileSelector.test.tsx
+++ src/components/FileSelector.test.tsx
@@ -1,11 +1,70 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
 import FileSelector from './FileSelector';
 
-test('renders upload button',() => {
-  render(<FileSelector/>);
-  const linkElement = screen.getByTestId('json-input');
-  expect(linkElement).toBeInTheDocument();
-  expect(linkElement).toHaveAttribute("type", "file")
-  expect(linkElement).toHaveAttribute("accept", "application/json")
+describe('File Selection', () => {
+
+  test('renders upload button', () => {
+    render(<FileSelector />);
+    const linkElement = screen.getByTestId('json-input');
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("type", "file")
+    expect(linkElement).toHaveAttribute("accept", "application/json")
+  });
+
+  describe('Json validation', () => {
+    test('valid Json', async () => {
+      let validJson = '{"valid":"json"}'
+      let file = new File([validJson], "valid.json", {type: "application/json"});
+      render(<FileSelector />)
+
+      let uploader = screen.getByTestId('json-input')
+      await waitFor(() =>
+        fireEvent.change(uploader, {
+          target: {files: [file]},
+        })
+      )
+
+      await waitFor(() =>
+        expect(screen.getByText(validJson)).toBeInTheDocument()
+      )
+
+    });
+    test('empty Json', async () => {
+      let file = new File([''], "empty.json", {type: "application/json"});
+      render(<FileSelector />)
+
+      let uploader = screen.getByTestId('json-input')
+      await waitFor(() =>
+        fireEvent.change(uploader, {
+          target: {files: [file]},
+        })
+      )
+
+      await waitFor(() =>
+        expect(screen.getByText('null')).toBeInTheDocument()
+      )
+
+    })
+
+    test('invalid Json', async () => {
+      let invalidJson = 'invalid json'
+      let file = new File([invalidJson], "valid.json", {type: "application/json"});
+      render(<FileSelector />)
+
+      let uploader = screen.getByTestId('json-input')
+      await waitFor(() =>
+        fireEvent.change(uploader, {
+          target: {files: [file]},
+        })
+      )
+
+      await waitFor(() =>
+        expect(screen.getByText('null')).toBeInTheDocument()
+      )
+
+    })
+  });
+
+
 });
--- src/components/FileSelector.tsx
+++ src/components/FileSelector.tsx
@@ -12,30 +12,49 @@ import React, {Component} from 'react';
 
 class FileSelector extends Component {
   state = {
-    selectedFile: null
+    tgExport: null
   };
-
-
-  onFileChange = (event: any) => {
-    //Validate File selected
-    this.setState({selectedFile: event.target.files[0]});
-
-  }
 
   render() {
 
     return (
       <div>
-        <input 
+        <input
           type="file"
-          onChange={this.onFileChange} 
-          accept="application/json" 
+          onChange={this.onFileChange}
+          accept="application/json"
           data-testid="json-input"
-          />
+        />
+        {JSON.stringify(this.state.tgExport)}
       </div>
     );
-
   }
+
+  onFileChange = (event: any) => {
+    let fr = new FileReader();
+
+    fr.onload = (e) => {
+      let buf = e.target?.result?.toString()
+      let tgExport = this.getTGExport(buf ? buf : "");
+      this.setState({tgExport: tgExport})
+    }
+
+    fr.readAsText(event.target.files[0]);
+  }
+
+  getTGExport = (input: string): any => {
+    let obj = null;
+    if (!input) {
+      return obj;
+    }
+
+    try {
+      obj = JSON.parse(input)
+    } catch (ex) {
+    }
+    return obj
+  }
+
 
 }
 
