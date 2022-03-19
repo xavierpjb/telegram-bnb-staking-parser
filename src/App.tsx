307fc39b9f9ae52d0bb85f5dab7b0f5354e1edc8
Xavier Jean Baptiste <31086996+xapatjb4@users.noreply.github.com>
2022-03-19 10:26:36 -0400

    Remove react base proj (#20)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {log4TSProvider} from "./config/LogConfig";
 import Home from './components/Home';
@@ -9,24 +8,8 @@ function App() {
   log.trace("Starting app")
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+    <div>
       <Home></Home>
-
     </div>
   );
 }
