ad6b448c72c27e159b37b6de1ea8f2eb8d3eafbb
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2022-03-20 12:56:50 -0400

    Add styling to instruction steps

--- src/components/TelegramExportInstructions.tsx
+++ src/components/TelegramExportInstructions.tsx
@@ -6,29 +6,34 @@ class TelegramExportInstructions extends Component {
   render() {
     log.debug("render export instructions")
     return (
-      <ul>
-        <li>
-          Download <a href="https://desktop.telegram.org/">telegram for desktop </a>
-          (Note: export feature is not available on telegram from mac app store, 
-          so you have to dowload the desktop version)
-        </li>
-        <li>
-          Select BSC staking bot chat
-        </li>
-        <li>
-          Select settings (3 dot located in the upper right corner)
-        </li>
-        <li>
-          Select "Export Chat History" and ensure that everything is unselected
-        </li>
-        <li>
-          Click Format and Select "Machine-readable JSON" and set path to where
-          you want the file to be dowloaded
-        </li>
-        <li>
-          Click export and wait for download to finish
-        </li>
-      </ul>
+      <li className="pt-5">
+        <div className="bg-white p-5 rounded-xl shadow-lg">
+          <h3 className="font-bold text-xl mb-2">Export Telegram chat to json</h3>
+          <ul className="p-2 list-disc">
+            <li>
+              Download <a href="https://desktop.telegram.org/">telegram for desktop </a>
+              (Note: export feature is not available on telegram from mac app store,
+              so you have to dowload the desktop version)
+            </li>
+            <li>
+              Select BSC staking bot chat
+            </li>
+            <li>
+              Select settings (3 dots located in the upper right corner)
+            </li>
+            <li>
+              Select "Export Chat History" and ensure that everything is unselected
+            </li>
+            <li>
+              Click Format and Select "Machine-readable JSON" and set path to where
+              you want the file to be dowloaded
+            </li>
+            <li>
+              Click export and wait for download to finish
+            </li>
+          </ul>
+        </div>
+      </li>
     )
   }
 }
