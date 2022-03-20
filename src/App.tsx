c6ffffd8b40a7b587bcd6ff25fa5317e6acbd347
Xavier Jean Baptiste <31086996+xapatjb4@users.noreply.github.com>
2022-03-20 12:59:31 -0400

    Add styling to instruction steps (#25)

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,18 @@ function App() {
   log.trace("Starting app")
 
   return (
-    <div>
-      This app assumes that you set up your telegram staking notifications with
-      @BSC_Binance_Chain_Bot
-      <ol>
-        <li>
-          <TelegramExportInstructions />
-        </li>
+    <div className="bg-amber-300 p-10">
+      <h2 className="font-bold text-4xl">
+        BNB Telegram staking tax csv generator 
+      </h2>
+
+      <p className="pt-2">
+        Generate a csv file from Telegram's @BSC_Binance_Chain_Bot
+        staking reward notifications.
+      </p>
+
+      <ol className="list-decimal">
+        <TelegramExportInstructions />
         <Home />
       </ol>
     </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,17 +11,25 @@ class Home extends Component {
   render() {
     log.debug('Render home')
     return (
-      <div>
-        <li>
-          <FileSelector
-            onValidTGExport={this.onValidTGExport}
-            onInvalidTGExport={this.onInvalidTGExport}
-          />
+      <div >
+        <li className="pt-5">
+          <div className="bg-white p-5 rounded-xl shadow-lg">
+            <h3 className="font-bold text-xl mb-2">Import the json file</h3>
+            <FileSelector
+              onValidTGExport={this.onValidTGExport}
+              onInvalidTGExport={this.onInvalidTGExport}
+            />
+          </div>
         </li>
 
         {this.state.tgMessages.length === 0 ? null :
-          <li>
-            <JsonProcessor tgMessages={this.state.tgMessages} />
+          <li className="pt-5">
+            <div className="bg-white p-5 rounded-xl shadow-lg">
+              <h3 className="font-bold text-xl mb-2">
+                Select staking year to export
+              </h3>
+              <JsonProcessor tgMessages={this.state.tgMessages} />
+            </div>
           </li>
         }
       </div>
